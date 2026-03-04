31953b6c31707c9f03b84f4fd78b63e19a6ffe26
Lina Marcela Avila Moreno <limavilam@hotmail.com>
2026-03-04 15:42:08 -0500

    Modificando el código de controller y app properties

--- infra/src/main/java/org/gpc/template/adapters/in/http/FavoriteControllerAdapter.java
+++ infra/src/main/java/org/gpc/template/adapters/in/http/FavoriteControllerAdapter.java
@@ -10,17 +10,17 @@ import org.gpc.template.handlers.GetFavoriteHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
 @RestController
 @AllArgsConstructor
+@CrossOrigin(
+        origins = "http://localhost:5173",
+        methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE},
+        allowedHeaders = {"Content-Type", "Authorization"}
+)
 public class FavoriteControllerAdapter {
   private final CreateFavoriteHandler createFavoriteHandler;
   private final GetFavoriteHandler getFavoriteHandler;
